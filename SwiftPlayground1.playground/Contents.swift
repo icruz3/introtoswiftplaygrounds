305ee718ebd74d4938b9d627475b745144573fc7
icruz3 <59888011+icruz3@users.noreply.github.com>
2020-01-21 10:34:24 -1000

    Week 2
    completed parts 1-7

--- SwiftPlayground1.playground/Contents.swift
+++ SwiftPlayground1.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//var myFirstVariable
+var myFirstVariable = "I've created my first string"
+
+//Declare and assign variable in singe line of code
+var mySecondVariable = "and look I added two strings together"
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+
+print(myThirdVariable)
